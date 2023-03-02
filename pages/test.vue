58612143a089a0bbfde3e6f5dfa24fa0f4ce7045
Mert Yildirim <mert.yildirim@INSTRPSD1002.local>
2023-03-02 16:57:18 +0300

    Google Auth .v1

---
+++ pages/test.vue
@@ -0,0 +1,43 @@
+<template>
+  <div id="googleButton"></div>
+</template>
+
+<script>
+export default {
+  mounted() {
+    // initialize Google Sign in
+    google.accounts.id.initialize({
+      client_id: 'YOUR_CLIENT_ID',
+      callback: this.handleCredentialResponse, //method to run after user clicks the Google sign in button
+      context: 'signin'
+    })
+
+    // render button
+    google.accounts.id.renderButton(
+        document.getElementById('googleButton'),
+        {
+          type: 'standard',
+          size: 'large',
+          text: 'signin_with',
+          shape: 'rectangular',
+          logo_alignment: 'center',
+          width: 250
+        }
+    )
+  },
+
+  methods: {
+    handleCredentialResponse(response) {
+      console.log(response);
+      // call your backend API here
+
+      // the token can be accessed as: response.credential
+    }
+  }
+}
+</script>
+
+
+<style scoped>
+
+</style>
